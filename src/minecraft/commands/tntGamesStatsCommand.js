f148b0b7ff04016de33eaa2a158c30449e6dfed7
vaivai <138060923+novyne@users.noreply.github.com>
2026-07-11 10:37:24 +0100

    Add TNT games stats command

---
+++ src/minecraft/commands/tntGamesStatsCommand.js
@@ -0,0 +1,94 @@
+const { formatNumber, formatError } = require("../../contracts/helperFunctions.js");
+const minecraftCommand = require("../../contracts/minecraftCommand.js");
+const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
+
+class TNTGamesStatsCommand extends minecraftCommand {
+  /** @param {import("minecraft-protocol").Client} minecraft */
+    constructor(minecraft) {
+        super(minecraft);
+
+        this.name = "tntgames";
+        this.aliases = ["tnt"];
+        this.description = "TNT Games stats of specified user. Defaults to sender if no user is specified.";
+        this.options = [
+            {
+                name: "username",
+                description: "Minecraft username",
+                required: false
+            },
+            {
+                name: "game",
+                description: "Type of game",
+                required: false
+            }
+        ];
+    }
+
+  /**
+   * @param {string} player
+   * @param {string} message
+   * */
+  async onCommand(player, message) {
+    
+    try {
+
+        const gameAliases = {
+            "tntrun" : "tntrun",
+            "pvprun" : "pvprun",
+            "pvp" : "pvprun",
+            "tnttag" : "tnttag",
+            "tag" : "tnttag",
+            "bowspleef" : "bowspleef",
+            "bow" : "bowspleef",
+            "wizards" : "wizards",
+            "wizard" : "wizards",
+            "wz" : "wizards"
+
+        }
+
+        
+        // argument handling : remember order is dynamic
+        const args = this.getArgs(message);
+        // first look for anything in gameAliases, default to tntrun
+        const gameArg = args.find(arg => Object.keys(gameAliases).includes(arg.toLowerCase()));
+        const game = gameArg ? gameAliases[gameArg.toLowerCase()] : "tntrun";
+
+
+        // then look for a player name (any remaining argument)
+        const targetPlayer = args.find(arg => !Object.keys(gameAliases).includes(arg.toLowerCase())) || player;
+        const hypixelPlayer = await hypixel.getPlayer(targetPlayer); 
+        if (!hypixelPlayer) throw "Player not found.";
+
+        const stats = hypixelPlayer.stats?.tntgames;
+        if (!stats || Object.keys(stats).length === 0) { 
+            throw `${hypixelPlayer.nickname} has never played TNT games.`; 
+        }
+        
+        const winstreak = stats.winstreak;
+
+        if (game === "tntrun") { 
+            const { wins, deaths } = stats.tntrun;
+            this.send(`${hypixelPlayer.nickname}'s TNT Run: W ${wins} L ${deaths} WS ${winstreak}`);
+        } else if (game === "pvprun") { 
+            const { kills, wins, deaths } = stats.pvprun;
+            this.send(`${hypixelPlayer.nickname}'s PvP Run: K ${kills} W ${wins} L ${deaths} WS ${winstreak}`);
+        } else if (game === "tnttag") { 
+            const { wins } = stats.tnttag;
+            this.send(`${hypixelPlayer.nickname}'s TNT Tag: W ${wins} WS ${winstreak}`);
+        } else if (game === "bowspleef") { 
+            const { wins, deaths } = stats.bowspleef;
+            this.send(`${hypixelPlayer.nickname}'s Bow Spleef: W ${wins} L ${deaths} WS ${winstreak}`);
+        } else if (game === "wizards") { 
+            const { wins, assists, KDRatio, deaths } = stats.wizards;
+            this.send(`${hypixelPlayer.nickname}'s Wizards: W ${wins} A ${assists} KDR ${KDRatio} D ${deaths}`);
+        }
+       
+    } catch (error) { 
+        this.send(formatError(error));
+    } 
+  }
+}
+
+module.exports = TNTGamesStatsCommand;
+
+// COMMAND : node scipts/commandHarness.js
