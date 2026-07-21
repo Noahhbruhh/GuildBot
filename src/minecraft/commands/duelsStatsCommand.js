d64e8b4e69e4aa429976b2c30e051793c8e728cb
vaivai <138060923+novyne@users.noreply.github.com>
2026-07-21 19:58:39 +0100

    Add WLR gain per win to duel ratio

--- src/minecraft/commands/duelsStatsCommand.js
+++ src/minecraft/commands/duelsStatsCommand.js
@@ -211,7 +211,7 @@ class DuelsStatsCommand extends minecraftCommand {
         const winIncrease = nextWins - wins;
         const pctWinIncrease = winIncrease / wins * 100;
 
-        return this.send(`${prefix} [${division}] ${hypixelPlayer.nickname} Next WLR : ${nextWLR} (${differenceText}) | Wins at next WLR : ${nextWins} (+${winIncrease} / ${pctWinIncrease.toFixed(1)}%)`)
+        return this.send(`${prefix} [${division}] ${hypixelPlayer.nickname} Next WLR : ${nextWLR} (${differenceText}) | Wins at next WLR : ${nextWins} (+${winIncrease} / ${pctWinIncrease.toFixed(1)}%) | +${(1 / losses).toPrecision(2)} WLR per win`)
       } // return gateway! we dont need to `} else {`
       
       const winstreakText = bestWinstreak === 0 
