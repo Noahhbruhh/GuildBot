8dacaae0d885666f1ffef2368c16235a35116f67
vaivai <138060923+novyne@users.noreply.github.com>
2026-08-03 17:08:48 +0100

    Add scaled GEXP and guild owner to GuildCommand

--- src/minecraft/commands/guildCommand.js
+++ src/minecraft/commands/guildCommand.js
@@ -1,6 +1,7 @@
 const minecraftCommand = require("../../contracts/minecraftCommand.js");
 const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
-const { formatNumber, formatError, titleCase } = require("../../contracts/helperFunctions.js");
+const { formatNumber, formatError, scaledGEXP } = require("../../contracts/helperFunctions.js");
+const { getUsername } = require("../../contracts/API/mowojangAPI.js")
 
 class GuildInformationCommand extends minecraftCommand {
   /** @param {import("minecraft-protocol").Client} minecraft */
@@ -37,10 +38,15 @@ class GuildInformationCommand extends minecraftCommand {
         guild = await hypixel.getGuild("name", args.join(" "), {noCaching: false});
       }
 
+      const guildMasterUUID = guild.members[0].uuid;
+      const guildMasterUsername = await getUsername(guildMasterUUID);
       const tagMessage = guild.tag ? `[${guild.tag}] ` : "";
 
+      // Scale all exp in exp history then sum
+      const scaledWeeklyGexp = guild.expHistory.map((datum) => scaledGEXP(datum.exp)).reduce((a, b) => a + b, 0);
+
       this.send(
-        `${tagMessage}${guild.name} | ${guild.members.length} members | LEVEL ${guild.level} | ${formatNumber(guild.totalWeeklyGexp)} Weekly GEXP`
+        `${tagMessage}${guild.name} | ${guild.members.length} members, owned by ${guildMasterUsername} | LEVEL ${guild.level} | ${formatNumber(guild.totalWeeklyGexp)} Raw WGEXP ${formatNumber(scaledWeeklyGexp)} Scaled WGEXP`
       );
     } catch (error) {
       this.send(formatError(error));
