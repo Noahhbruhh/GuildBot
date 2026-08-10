76964f817dd59a589f8a59a20e0b4fa81c351e3e
vaivai <138060923+novyne@users.noreply.github.com>
2026-08-10 16:55:03 +0100

    Add !guild invocation to PlayerOverviewCommand

--- src/minecraft/commands/playerOverviewCommand.js
+++ src/minecraft/commands/playerOverviewCommand.js
@@ -1,10 +1,12 @@
 const minecraftCommand = require("../../contracts/minecraftCommand.js");
+const hypixel = require("../../contracts/API/HypixelRebornAPI.js");
 const { delay } = require("../../contracts/helperFunctions.js");
 const { getUUID } = require("../../contracts/API/mowojangAPI.js");
 
 const TitlesCommand = require("./duelsTitlesCommand.js");
 const UrchinCommand = require("./urchinCommand.js");
 const PlayerCommand = require("./playerCommand.js");
+const GuildCommand = require("./guildCommand.js");
 
 class PlayerOverviewCommand extends minecraftCommand {
   /** @param {import("minecraft-protocol").Client} minecraft */
@@ -13,7 +15,7 @@ class PlayerOverviewCommand extends minecraftCommand {
 
     this.name = "overview";
     this.aliases = [];
-    this.description = "Shows an overview for a specified player by running informative commands. Invokes !titles, !urchin, and !player.";
+    this.description = "Shows an overview for a specified player by running informative commands. Invokes !titles, !urchin, !player and !guild commands.";
     this.options = [
       {
         name: "player",
@@ -31,22 +33,32 @@ class PlayerOverviewCommand extends minecraftCommand {
     try {
       const args = this.getArgs(message);
       const targetPlayer = args[0] || player;
-
+      
       // first check if the target player exists
       const targetPlayerUUID = await getUUID(targetPlayer);
       if (!targetPlayerUUID) {
         return this.send(`[ERROR] Player ${targetPlayer} not found.`);
       }
 
+      const playerCommand = new PlayerCommand(this.minecraft);
+      const guildCommand = new GuildCommand(this.minecraft);
       const titlesCommand = new TitlesCommand(this.minecraft);
       const urchinCommand = new UrchinCommand(this.minecraft);
-      const playerCommand = new PlayerCommand(this.minecraft);
 
-      await titlesCommand.onCommand(targetPlayer, message);
-      await delay(1000);
-      await urchinCommand.onCommand(targetPlayer, message);
-      await delay(1000);
-      await playerCommand.onCommand(targetPlayer, message);
+      const guild = await hypixel.getGuild("player", targetPlayer, { noCaching: false });
+
+      const commands = [
+        playerCommand,
+        guild ? guildCommand : null,
+        titlesCommand,
+        urchinCommand,
+      ].filter(Boolean);
+
+      for (const command of commands) {
+        if (!command) { continue; } // error resolving! this is already handled in the filter
+        await command.onCommand(targetPlayer, targetPlayer);
+        await delay(1000);
+      }
       
     } catch (error) {
       this.send(`[ERROR] ${error}`);
